b7c8523382a92ae4a4db480459f9f5bb8035fd31
Franco Jesus Cahua Soto <fcahua@unsa.edu.pe>
2024-06-07 17:27:12 -0500

    Creacion de otro metodo another view en la app inicio y el mapeo de su url

--- django-copiaGIT/src/listaContactos/urls.py
+++ django-copiaGIT/src/listaContactos/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from inicio.views import myHomeView
+from inicio.views import myHomeView, anotherView
 
 urlpatterns = [
     path('', myHomeView, name="HomePage"),
+    path('another/', anotherView),
     path('admin/', admin.site.urls),
 ]
