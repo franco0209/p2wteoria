c849a902a2e790ccf806141e9362cbe4a28d737a
Franco Jesus Cahua Soto <fcahua@unsa.edu.pe>
2024-06-15 23:13:55 -0500

    Se crea template nuevo createview, se modifican las vistas

--- django-copiaGIT/src/personas/views.py
+++ django-copiaGIT/src/personas/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Persona
+from .forms import PersonaForm
 # Create your views here.
 def personaTestView(request):
     obj=Persona.objects.get(id=1)
     context={
         "objeto":obj
     }
-    return render(request, "personas/test.html", context)
+    return render(request, "personas/descripcion.html", context)
+def personaCreateView(request):
+    form=PersonaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
